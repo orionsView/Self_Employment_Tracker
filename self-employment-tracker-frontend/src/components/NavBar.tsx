3c76ebc8b2d20c682345bb0ef39f45cc49606d2c
orionsView <davidhunterphillips03@gmail.com>
2025-07-28 12:01:48 -0700

    update

--- self-employment-tracker-frontend/src/components/NavBar.tsx
+++ self-employment-tracker-frontend/src/components/NavBar.tsx
@@ -1,13 +1,15 @@
+import FistLogo from "../assets/fist-logo.svg"
+import MenuLogo from "../assets/menu-logo.svg"
 function NavBar() {
     return (
         <>
             <div className="flex justify-between items-center w-[100%] h-[10%] border-b-2">
                 <div className="flex flex-row items-center">
-                    <img className="w-12" src="/src/assets/fist-logo.svg" alt="fist" />
+                    <img className="w-12" src={FistLogo} alt="fist" />
                     <p className="text-[6vw] font-bold">Punch The Clock</p>
                 </div>
 
-                <a href="/menu"><img className="w-12" src="/src/assets/menu-logo.svg" alt="menu" /></a>
+                <a href="/menu"><img className="w-12" src={MenuLogo} alt="menu" /></a>
             </div>
         </>
     )
