2d52b09fea2d09e4a40e00fa2f61d8b3a62ea7d1
orionsView <davidhunterphillips03@gmail.com>
2025-10-25 13:03:53 -0700

    fixed icon issues

--- self-employment-tracker-frontend/src/components/MapInputField.tsx
+++ self-employment-tracker-frontend/src/components/MapInputField.tsx
@@ -1,6 +1,18 @@
 import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
 import { useState } from "react";
+import L from "leaflet";
+
+// Fix marker icons (important for Vite, React, or CRA)
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+    iconUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+    shadowUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
 
 function MapInputField() {
     // const position: [number, number] = [47.6062, -122.3321];
