7e9fc98ae9995c269cd278fcc170e3c06a6e710b
orionsView <davidhunterphillips03@gmail.com>
2025-11-14 16:27:47 -0800

    changed font styles

--- self-employment-tracker-frontend/src/components/NavBar.tsx
+++ self-employment-tracker-frontend/src/components/NavBar.tsx
@@ -6,7 +6,7 @@ function NavBar({ showMenu = true }: { showMenu?: boolean }) {
             <div className="flex justify-between items-center w-[100%] h-[10%] border-b-2">
                 <a href="/menu" className="flex flex-row items-center bg-mainColor rounded-3xl p-2 h-[80%]" >
                     <img className="w-12" src={FistLogo} alt="fist" />
-                    <p className="text-[7vw] font-bold text-tertiaryColor">Punch The Clock</p>
+                    <p className="text-[7vw] text-tertiaryColor header-font">Punch The Clock</p>
                 </a>
 
                 {showMenu && <a href="/menu"><img className="w-16 bg-mainColor rounded-xl h-[80%] p-2" src={MenuLogo} alt="menu" /></a>}
